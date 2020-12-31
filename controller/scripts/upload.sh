d433a9743c6dee4e4e0f94b06508baaa2dbb3b57
Richard Pianka <richard@piankalabs.com>
2020-12-30 21:37:02 -0500

    adding date to upload script

--- controller/scripts/upload.sh
+++ controller/scripts/upload.sh
@@ -3,3 +3,5 @@
 scp *.ino *.h *.cpp pi@piankabot.lan:/home/pi/controller
 ssh pi@piankabot.lan '/home/pi/bin/arduino-cli compile -b arduino:avr:mega /home/pi/controller'
 ssh pi@piankabot.lan '/home/pi/bin/arduino-cli upload -b arduino:avr:mega -p /dev/ttyACM0 /home/pi/controller'
+
+date
